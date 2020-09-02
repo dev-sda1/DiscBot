cc474634c1a90922a9831fbae43b9f165780ca81
dev-sda1 <virtualhatrblx@gmail.com>
2020-09-02 22:22:09 +0100

    small edit to ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -3,7 +3,7 @@ module.exports={
     category: "info",
     description: "Returns the server latency, and API latency",
     run: async(client,message)=>{
-        const msg = await message.channel.send(`Pinging servers..`);
+        const msg = await message.channel.send(`Pinging..`);
         await msg.edit(`Pong!\nServer Latency: ${Math.floor(msg.createdAt - message.createdAt)}ms\nAPI Latency: ${Math.round(client.ping)}ms`);
     }
 }
