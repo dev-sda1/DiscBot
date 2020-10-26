b875d986379d0ecd0dac124874b3955839848982
dev-sda1 <43112896+dev-sda1@users.noreply.github.com>
2020-10-26 17:31:02 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,11 +2,17 @@
 const{Client, RichEmbed, Collection} = require("discord.js");
 const config = require("./storage/config.json");
 const fs = require("fs");
+const mongoose = require("mongoose");
+const settings_model = require("./models/config.js")
 //mongodb maybe in the near future.
 
 
 //Client settings
 const settings = require("./storage/server_settings.json");
+const server_rankings = require("./storage/ranks.json"); //These are here for later reference, dunno what to do with them yet.
+const warnings = require("./storage/warnings.json");
+const tickets = require("./storage/tickets.json");
+const { exit } = require("process");
 
 
 //Preparing the bot for use
@@ -21,13 +27,33 @@ client.aliases = new Collection();
     require(`./handlers/${handler}`)(client);
 });
 
+
+
+// //Command handling (not used anymore but kept as a backup)
+// fs.readdir("./commands/", (err,file)=>{
+//     if(err) console.log(err); //if error it logs it in console because fuck doing actual responses
+
+//     let jsfile = file.filter(f => f.split(".").pop() === "js");
+//     if(jsfile.length <=0){
+//         console.log("Commands couldn't be found."); //If the folder either doesn't exist or is empty
+//         return;
+//     }
+
+//     jsfile.forEach((f, i)=>{
+//         let props = require(`./commands/${f}`);
+//         console.log(`${f} loaded`);
+//         client.commands.set(props.help.name, props);
+//     });
+// });
+
+
 //Bot
 client.on("ready", async()=>{
-    console.log(`\nHi! ${client.user.username} is now online!`);
+    console.log(`\nLogin successful as: ${client.user.username}`);
 
     client.user.setPresence({
         game:{
-            name: 'trains at Moor St',
+            name: 'you',
             type: "Watching",
             url: "https://dev.sda.one"
         }
@@ -36,19 +62,44 @@ client.on("ready", async()=>{
 
 client.on("guildCreate", (guild)=>{
     console.log("Added to a server? How?");
-    if(!settings[guild.id]){
-        settings[guild.id] = {
-            prefix: ".",
-            mod_logs: "",
-            greetingschannel: "",
-            joinmsg: " has joined.",
-            leavemsg: " has left",
-            banmsg: " has been banned :hammer:"
+    //DB Checking to see if there's even a config lol
+    settings_model.findOne({
+        "_id": guild.id
+    }, (err,result)=>{
+        if(result){
+         //somehow do some stuff here lol
+        }else if (!result || err){
+            //create the config or something idk
+            const guild_stuff = new settings_model({
+                _id: guild.id,
+                log_channel: "",
+                greetingschannel: "",
+                joinmsg: " has joined.",
+                leavemsg: " has left",
+                banmsg: " has been banned :hammer:"
+            })
+
+            guild_stuff.save()
+                .then(result => console.log(result))
+                .catch(err => console.log(err));
+
         }
-    }
-    fs.writeFile("./storage/server_settings.json", JSON.stringify(settings, null, 2), (err)=>{
-        if(err) console.warn(`[SE-SETTINGS] Error creating guild information: ${err}`);
     })
+
+
+    //if(!settings[guild.id]){
+    //    settings[guild.id] = {
+    //        prefix: ".",
+    //        mod_logs: "",
+    //        greetingschannel: "",
+    //        joinmsg: " has joined.",
+    //        leavemsg: " has left",
+    //        banmsg: " has been banned :hammer:"
+    //    }
+    //}
+    //fs.writeFile("./storage/server_settings.json", JSON.stringify(settings, null, 2), (err)=>{
+    //    if(err) console.warn(`[SE-SETTINGS] Error creating guild information: ${err}`);
+    //})
 })
 
 client.on("guildDelete", (guild)=>{
@@ -68,7 +119,7 @@ client.on("guildMemberAdd", member => {
 })
 
 client.on("message", async message=>{
-    let prefix = ">";
+    let prefix = config.prefix || ">";
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
@@ -90,15 +141,38 @@ client.on("message", async message=>{
      }
 });
 
+//Check if anything from the configuration file is missing.
+if(config.token == null){
+    console.log('\x1b[31m', `ERROR: Token not found. Get your bot token from discord.com/developers/applications`);
+    exit();
+}
 
-//Logging into the bot hopefully.
-//client.login(config.token);
+if(config.mongodb_password || config.mongodb_url || config.mongodb_username == null){
+    console.log('\x1b[31m', `ERROR: One or more mongodb fields in your config.json are empty.`)
+}
 
-var argv = require('minimist')(process.argv.slice(2));
-console.log(argv);
+//Checking if mongodb connects successfully
+try{
+    mongoose.connect(config.mongodb_url,
+        {
+            "auth": {
+            "authSource": "admin"
+            },
+        "user": config.mongodb_username,
+        "pass": config.mongodb_password,
+        useNewUrlParser: true
+    });
+}catch(e){
+    console.log('\x1b[31m', `ERROR: Mongodb connection failed. Check the details you added in your config.json and try again.\nFull details: ${e}`);
+    exit();
+}
 
-// node bot.js --token [here]
-// node bot.js --debug
-// node bot.js
+console.log("Logging in..");
+try{
+    client.login(config.token);
+}catch(e){
+    console.log('\x1b[31m', "ERROR: Login failed. Check your token.");
+    exit();
+}
 
 
